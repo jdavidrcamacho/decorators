ae7cc114f7e7b879bf82c9926363e31b35c4972d
jdavidrcamacho <jdavidrcamacho@gmail.com>
2022-08-31 17:26:34 +0100

    files

---
+++ decorators/decs.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+import time
+
+
+def timeit_func(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        # first item in the args, ie `args[0]` is `self`
+        print(f'Function {func.__name__} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+def timeit_method(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print(f'{method.__name__} - {(te - ts) * 1000} ms')
+        return result
+    return timed
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='decorators',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Just a decorator thing',
+    author='Camacho',
+    license='',
+    # packages=['decorators'],
+    install_requires=[
+        'numpy', 
+      ],
+)
